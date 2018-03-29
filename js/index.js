cdf78ef1a50f3d31eba94b690b3ce8f02a15a32b
Stijn van Schooten <svsoke@hotmail.com>
2018-03-29 21:00:26 +0200

    project -> merchandise and finished + improved layout

--- js/index.js
+++ js/index.js
@@ -1,62 +1,23 @@
 // var baseURI = 'https://raw.githubusercontent.com/svanschooten/lltnf/master/data/';
 var baseURI = 'http://127.0.0.1:8080/data/';
 
-lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': true,
-    'imageFadeDuration': 300,
-    'fadeDuration': 300
-});
-
 var application = new Vue({
     el: '#application',
     data: {
         page: 'home',
-        project: null,
-        projects: [],
+        merchandise: [],
         people: [],
         home: {
             video: "",
             text: ""
         },
         error: {
-            projects: "",
-            people: ""
-        }
-    },
-    methods: {
-        navToProject: function (project) {
-            this.project = project;
-            this.page = 'project';
+            home: "",
+            people: "",
+            merchandise: ""
         }
     }
 });
-
-$.get(baseURI + 'projects.json')
-    .done(function (data) {
-        application.error.projects = "";
-        try {
-            data = JSON.parse(data);
-        } catch (_) {}
-        application.projects = data.map(function (project) {
-            project.hover = false;
-            project.image = 'url(' + baseURI + 'images/' + project.image + ')';
-            project.images = project.images.map(function (image) {
-                return baseURI + 'images/' + image;
-            }).shuffle();
-            project.top = (Math.floor(Math.random() * 200) - 100) + 'px';
-            project.left = Math.floor(Math.random() * 80) + '%';
-            return project;
-        }).shuffle();
-    })
-    .fail(function (xhr, status, error) {
-        if (xhr.responseJSON && xhr.responseJSON.message && xhr.responseJSON.message != application.error.people) {
-            application.error.projects = xhr.responseJSON.message;
-        }
-        console.log(xhr);
-        console.log(status);
-        console.log(error);
-    });
 
 $.get(baseURI + "people.json")
     .done(function (data) {
@@ -65,13 +26,33 @@ $.get(baseURI + "people.json")
             data = JSON.parse(data);
         } catch (_) {}
         application.people = data.map(function (person) {
-            person.image = 'url(' + baseURI + 'images/' + person.image + ')';
+            person.image = baseURI + 'images/' + person.image;
             return person;
         });
     })
     .fail(function (xhr, status, error) {
-        if (xhr.responseJSON && xhr.responseJSON.message && xhr.responseJSON.message != application.error.projects) {
+        if (xhr.responseJSON && xhr.responseJSON.message && xhr.responseJSON.message !== application.error.people) {
             application.error.people = xhr.responseJSON.message;
+        }
+        console.log(xhr);
+        console.log(status);
+        console.log(error);
+    });
+
+$.get(baseURI + "merchandise.json")
+    .done(function (data) {
+        application.error.merchandise = "";
+        try {
+            data = JSON.parse(data);
+        } catch (_) {}
+        application.merchandise = data.map(function (merch) {
+            merch.image = baseURI + 'images/' + merch.image;
+            return merch;
+        });
+    })
+    .fail(function (xhr, status, error) {
+        if (xhr.responseJSON && xhr.responseJSON.message && xhr.responseJSON.message !== application.error.merchandise) {
+            application.error.merchandise = xhr.responseJSON.message;
         }
         console.log(xhr);
         console.log(status);
@@ -88,7 +69,7 @@ $.get(baseURI + "home.json")
         application.home = data;
     })
     .fail(function (xhr, status, error) {
-        if (xhr.responseJSON && xhr.responseJSON.message && xhr.responseJSON.message != application.error.projects) {
+        if (xhr.responseJSON && xhr.responseJSON.message && xhr.responseJSON.message !== application.error.home) {
             application.error.home = xhr.responseJSON.message;
         }
         console.log(xhr);
